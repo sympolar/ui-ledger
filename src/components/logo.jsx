a9765eef46b799852987ad5462f13adf967659b4
sympolar <sympolardev@gmail.com>
2026-02-28 04:55:28 -0500

    Create logo.jsx

---
+++ src/components/logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <img
+      src="/assets/logo-bear.svg"
+      className="w-48 drop-shadow-[0_0_20px_rgba(79,174,214,0.35)]"
+      alt="Zengine Bear"
+    />
+  )
+}
